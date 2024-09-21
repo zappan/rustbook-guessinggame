2e2be6b96ae02ab552e95e98a25f2df38ad91f2c
Tomislav Capan <tomislav.capan@gmail.com>
2024-09-21 16:54:35 +0200

    Handling invalid user inputs

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io::{self, Write};
 
+const MAX_RAND: u8 = 100;
+
 fn read_user_input() -> String {
   // read user input here
   let mut user_input = String::new();
@@ -15,9 +17,9 @@ fn read_user_input() -> String {
 fn game_round(rng: &mut ThreadRng) {
   println!("\n=== Guess the number! ===");
 
-  // ## Generate a secret number between 1 and 100
-  // let random: u8 = (rand::random::<u8>() % 100) + 1;
-  let random: u8 = rng.gen_range(1..=100);
+  // ## Generate a secret number between 1 and MAX_RAND
+  // let random: u8 = (rand::random::<u8>() % MAX_RAND) + 1;
+  let random: u8 = rng.gen_range(1..=MAX_RAND);
 
   // println!("Random generated number is: {random}");
 
@@ -25,9 +27,20 @@ fn game_round(rng: &mut ThreadRng) {
     // ## User input a guess
     print!("Your guess: ");
     io::stdout().flush().unwrap();
-    let guess: u8 = read_user_input()
-      .parse()
-      .expect("Your guess must be a number between 1 and 100");
+
+    // ## Naive parsing which can cause panic by overflows and non-numeric inputs
+    // let guess: u8 = read_user_input()
+    //   .parse()
+    //   .expect("Your guess must be a number between 1 and {MAX_RAND}");
+
+    // ## Invalid input handling on user input parsing
+    let guess: u8 = match read_user_input().parse() {
+      Ok(val @ 1..=MAX_RAND) => val,
+      _ => {
+        println!("Your guess must be a number between 1 and {MAX_RAND}");
+        continue;
+      }
+    };
 
     // if guess == random {
     //   println!("Bravo! You guessed it!");
